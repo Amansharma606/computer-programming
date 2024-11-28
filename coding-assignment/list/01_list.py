7515f2a2613727d63331ac65bd8567c9bcd1bf5b
Amansharma606 <sharmaaman20412@gmail.com>
2024-11-28 19:18:41 +0530

    Update 01_list.py

--- coding-assignment/list/01_list.py
+++ coding-assignment/list/01_list.py
@@ -1,8 +1,6 @@
-# Question no 1 Reverse a string.
+# Question = 1 Find the maximum element in a list.
+my_list = [3, 1, 7, 4, 9, 2]
+max_element = max(my_list)
+print(max_element)  
 
-string = "hello"
-reversed_string = string[::-1]
-print(reversed_string) 
-
- 
 
